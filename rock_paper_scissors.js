2aa550440ab0a1e998d44d2ebd5da47a0e56ded0
Francisco-CB <francisco.cubillos@ug.uchile.cl>
2021-04-02 18:05:57 -0300

    Pequeño refactoring, también se agrega función playRound

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,8 +1,38 @@
-function playRound(){}
+function playRound(playerSelection, computerSelection){
+    let results = 'You ';
+    let playerResult;
 
-function computerPlay(){}
+    if (playerSelection == 'rock' && computerSelection == 'paper'){
+        playerResult = 'lose! Paper beats Rock';
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors'){
+        playerResult = 'win! Rock beats Scissors';
+    }
+    
+    else if (playerSelection == 'paper' && computerSelection == 'scissors'){
+        playerResult = 'lose! Scissors beats Paper';
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        playerResult = 'win! Paper beats Rock';
+    }
+    
+    else if (playerSelection == 'scissors' && computerSelection == 'rock'){
+        playerResult = 'lose! Rock beats Scissors';
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper'){
+        playerResult = 'win! Scissors beats Paper';
+    }
 
-function computerSelection(){
+    else {
+        playerResult = 'tied! No one wins';
+    }
+
+    results +=  playerResult;
+
+    return results
+}
+
+function computerPlay(){
     let computer_options = ['rock', 'paper', 'scissors'];
     let election = Math.floor(Math.random()*3);
     return computer_options[election]
@@ -13,3 +43,5 @@ function playerSelection(){
     player_election = window.prompt('Rock, Paper, or Scissors?');
     return player_election.toLowerCase()
 }
+
+function game(){}
